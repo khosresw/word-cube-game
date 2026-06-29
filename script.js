0a204dc759e0b45e5ab47b8ac4f4f222a042db78
khosresw <khosre.swati@gmail.com>
2026-06-29 17:43:22 +0530

    Update cube faces to use doodle pattern images
    Replaced 'floral_panel.svg' with 'doodle_pattern.svg' for all cube faces.

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ for(let i=0;i<3;i++){
 
         <div class="cube">
 
-            <div class="face front"><img src="floral_panel.svg" class="face-svg"></div>
-            <div class="face back"><img src="floral_panel.svg" class="face-svg"></div>
-            <div class="face left"><img src="floral_panel.svg" class="face-svg"></div>
-            <div class="face right"><img src="floral_panel.svg" class="face-svg"></div>
-            <div class="face top"><img src="floral_panel.svg" class="face-svg"></div>
-            <div class="face bottom"><img src="floral_panel.svg" class="face-svg"></div>
+            <div class="face front"><img src="doodle_pattern.svg" class="face-svg"></div>
+            <div class="face back"><img src="doodle_pattern.svg" class="face-svg"></div>
+            <div class="face left"><img src="doodle_pattern.svg" class="face-svg"></div>
+            <div class="face right"><img src="doodle_pattern.svg" class="face-svg"></div>
+            <div class="face top"><img src="doodle_pattern.svg" class="face-svg"></div>
+            <div class="face bottom"><img src="doodle_pattern.svg" class="face-svg"></div>
 
         </div>
 
